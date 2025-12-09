bf8bb878f26d098d56795bcee1104d20b22f9fc4
Md Tasmim Al Tahsin <tahsinniyan@gmail.com>
2025-12-09 18:10:05 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Footer />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            This is Navbar
+        </div>
+    );
+};
+
+export default Navbar;
